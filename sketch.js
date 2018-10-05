fbb40aa0d73669d9ba895b5ebcb68ee5b9980373
brunolorenz <lorenz.bruno@gmail.com>
2018-10-05 10:46:51 -0300

    Alonso Alvarez

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var letra1;
 var letra2;
 var letra3;
+var r = 89;
+var g = 76;
+var b = 41;
 
 function preload () {
-  myFont = loadFont('assets/BreeSerif-Regular.ttf')
+  myFont = loadFont('assets/Lora-Italic.ttf')
+  myFonttitulo = loadFont('assets/BreeSerif-Regular.ttf')
   frase1 = loadStrings("frases1.txt");
   frase2 = loadStrings("frases2.txt")
   frase3 = loadStrings("frases3.txt");
@@ -16,8 +20,19 @@ function setup() {
   textFont(myFont);
   fill(31,31,122,100);
   textSize(20);
-  text("no Ateliê de Artefatos Pós Normais da effêmera, basta pressionar a tela para criar seu próprio objeto",50,50,150);
+  textAlign(CENTER);
+  text("clique na tela",windowWidth/2,windowHeight/2);
 
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(255,253,234);
+  textFont(myFont);
+  fill(31,31,122,100);
+  textSize(20);
+  textAlign(CENTER);
+  text("clique na tela",windowWidth/2,windowHeight/2);
 }
 
 function touchStarted() {
@@ -29,21 +44,26 @@ function touchStarted() {
   var centrov = windowWidth/2;
   var centroh = windowHeight/2;
 
+
 textSize(40);
 textStyle(BOLD);
 textAlign(CENTER);
 textFont(myFont);
 background(255,253,234);
-fill(31,31,122,255);
+fill(r,g,b,255);
 text(linha1, centrov, centroh-50);
-fill(31,31,122,200);
+fill(r,g,b,200);
 text(linha2, centrov, centroh);
-fill(31,31,122,150);
+fill(r,g,b,150);
 text(linha3, centrov, centroh+50);
 
+textFont(myFonttitulo);
 textSize(12);
 fill(31,31,31,150);
 text("S E U  H A I K A I   nº "+round(catalogo), centrov, 50);
 
+}
 
+function keyReleased() {
+  saveCanvas();
 }
